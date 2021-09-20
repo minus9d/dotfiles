fc12cab8f28c00ba8b57040ceb295b030ceb02ac
minus9d <minus9d@gmail.com>
2021-09-20 20:41:15 +0900

    yasnippetを追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -305,6 +305,50 @@
   (add-to-list 'company-backends 'company-c-headers))
 
 
+;;;;;;;;;;;;;;;
+;; yasnippet ;;
+;;;;;;;;;;;;;;;
+
+;; https://mako-note.com/python-emacs-ide/#yasnippet
+;; TODO: yasnippet-snippets によるスニペットダウンロードに失敗する
+;; ようなので、https://github.com/AndreaCrotti/yasnippet-snippets
+;; から取得したファイルを~/.emacs.d/snippetsに手動で配置
+
+(leaf yasnippet
+  :ensure t
+  :blackout yas-minor-mode
+  :custom ((yas-indent-line . 'fixed)
+           (yas-global-mode . t)
+                                 )
+  :bind ((yas-keymap
+          ("<tab>" . nil))            ; conflict with company
+         (yas-minor-mode-map
+          ("C-c y i" . yas-insert-snippet)
+          ("C-c y n" . yas-new-snippet)
+          ("C-c y v" . yas-visit-snippet-file)
+          ("C-c y l" . yas-describe-tables)
+          ("C-c y g" . yas-reload-all)))
+  :config
+  (leaf yasnippet-snippets :ensure t)
+  (leaf yatemplate
+    :ensure t
+    :config
+    (yatemplate-fill-alist))
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  (defun set-yas-as-company-backend ()
+    (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+    )
+  :hook
+  ((company-mode-hook . set-yas-as-company-backend))
+  )
+
+
 ;;; 以下、leafを使わない設定
 
 
